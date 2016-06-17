9c7c148520fd0ec3fe2787277123d5dcd770b5b1
talhakosen <talhakosen@hotmail.com>
2016-06-17 08:35:36 +0300

    alarm

--- app/src/main/java/com/codepath/example/servicesnotificationsdemo/activities/MainActivity.java
+++ app/src/main/java/com/codepath/example/servicesnotificationsdemo/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.codepath.example.servicesnotificationsdemo.activities;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,11 +12,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.example.servicesnotificationsdemo.R;
+import com.codepath.example.servicesnotificationsdemo.receiver.MyAlarmReceiver;
 import com.codepath.example.servicesnotificationsdemo.receiver.MySimpleReceiver;
 import com.codepath.example.servicesnotificationsdemo.services.MySimpleService;
 
 public class MainActivity extends Activity {
 	public MySimpleReceiver receiverForSimple;
+	private PendingIntent alarmPendingIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,26 @@ public class MainActivity extends Activity {
 		i.putExtra("receiver", receiverForSimple);
 		// Start the service
 		startService(i);
+	}
+	public void onStartAlarm(View v) {
+		// Construct an intent that will execute the AlarmReceiver
+		Intent intent = new Intent(getApplicationContext(), MyAlarmReceiver.class);
+		intent.putExtra("receiver", receiverForSimple);
+		// Create a PendingIntent to be triggered when the alarm goes off
+		alarmPendingIntent = PendingIntent.getBroadcast(this, MyAlarmReceiver.REQUEST_CODE,
+				intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		// Setup periodic alarm every 5 seconds
+		long firstMillis = System.currentTimeMillis(); // first run of alarm is immediate
+		int intervalMillis = 5000; // 5 seconds
+		AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+		alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, intervalMillis, alarmPendingIntent);
+	}
+
+	public void onStopAlarm(View v) {
+		AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+		if (alarmPendingIntent != null) {
+			alarm.cancel(alarmPendingIntent);
+		}
 	}
 
 	@Override
